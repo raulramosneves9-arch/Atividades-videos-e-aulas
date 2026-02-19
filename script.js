ee24a528b69e079fc836435e86f12db85bd61622
raulramosneves9-arch <raulramosneves9@gmail.com>
2026-02-19 19:20:45 +0000

    ProgressBar

--- script.js
+++ script.js
@@ -1,17 +1,111 @@
 const songName = document.getElementById ('song-name');
-
+const bandName = document.getElementById ('band-name')
 const song = document.getElementById ('audio');
+const cover = document.getElementById ('cover')
 const play = document.getElementById ('play');
+const next = document.getElementById ('next');
+const previous = document.getElementById ('previous');
+const currentProgress = document.getElementById ('current-progress')
 
-songName.innerText = "Tempo Perdido";
 
+const Construção = {
+     songName: 'Construção',
+     artist: 'Chico Buarque',
+     file: 'Construção'
 
+};
+
+const Evidências = {
+     songName: 'Evidências',
+     artist: 'Chitãozinho e Xororó',
+     file: 'Evidências',
+
+};
+
+const Tempo_Perdido = {
+     songName: 'Tempo Perdido',
+     artist: 'Lergião Urbana',
+     file: 'Tempo_Perdido'
+
+};
+
+let isPlaying = false;
+const playlist = [Construção, Tempo_Perdido, Evidências]
+let index = 0;
 
 function playSong () {
-    play.querySelector('.bi').classList.toggle('bi-play-circle-fill');
-    play.querySelector('.bi').classList.toggle('bi-pause-circle-fill');
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
     song.play();
+    isPlaying = true;
 }
- play.addEventListener('click', playSong);
 
+function pauseSong () {
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider () {
+    if (isPlaying === true) {
+        pauseSong();
+    }
+
+    else {
+        playSong();
+    }
+}
+
+function initializeSong () {
+    cover.src = `images/${playlist[index].file}.jpg`; 
+    song.src = `songs/${playlist[index].file}.mp3`;
+    songName.innerText = playlist[index].songName
+    bandName.innerText = playlist[index].artist
+} 
+
+function privioussong () {
+    index--;
+    if (index < 0) {
+        index = playlist.length - 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+function previousSong () {
+    if (index === 0) {
+        index = playlist.length - 1;
+    }
+    else {
+    index -= 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+
+function nextSong () {
+    if (index === playlist.length - 1) {
+        index = 0;
+    }
+    else {
+    index += 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+
+function updateProgressBar (){
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+
+initializeSong();
+
+ play.addEventListener('click', playPauseDecider);
+ previous.addEventListener('click', previousSong);
+ next.addEventListener('click', nextSong);
+ song.addEventListener('timeuptade', )
  
